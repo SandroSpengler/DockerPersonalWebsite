c335151f2ed08697ba14d1a09cf7b245a3015c48
Sandro Spengler Macbook <sandro.spengler@web.de>
2021-11-22 22:28:28 +0100

    Setup for dev is working

---
+++ scripts/buildReactApp.sh
@@ -0,0 +1,11 @@
+reactAppPath="~/Dropbox/0_Webdevelopment/Projects/React/Live/PersonalWebsite"
+dockerAppPath="~/Dropbox/0_Webdevelopment/Docker/Live/PersonalWebsite"
+
+rm -rf ${dockerAppPath}/www
+
+mkdir ${dockerAppPath} www
+
+npm run build --prefix \
+    ${reactAppPath}
+
+cp ${reactAppPath}/build ${dockerAppPath}/www
