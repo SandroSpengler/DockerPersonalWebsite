163ab63bb4b72fae7b5a91aa5dc05c8e3e3fb447
Sandro Spengler <sandro.spengler@web.de>
2021-12-26 20:58:46 +0100

    Slight improvments

--- scripts/runDockerCompose.sh
+++ scripts/runDockerCompose.sh
@@ -1,8 +1,13 @@
-docker-compose down --remove-orphans -v
+docker-compose \
+    -f /home/sandro/Dropbox/0_Webdevelopment/Docker/Live/PersonalWebsite/docker-compose.yaml \
+    -f /home/sandro/Dropbox/0_Webdevelopment/Docker/Live/BackendServerMainDocker/docker-compose.yaml \
+    down --remove-orphans -v
 
 docker-compose \
     -f docker-compose.yaml \
     -f docker-compose.dev.yaml \
+    -f /home/sandro/Dropbox/0_Webdevelopment/Docker/Live/BackendServerMainDocker/docker-compose.yaml \
+    -f /home/sandro/Dropbox/0_Webdevelopment/Docker/Live/BackendServerMainDocker/docker-compose.dev.yaml \
     up -d \
     --build \
 
