702d9c3b209dfc24633893769775b94868594770
sandro spengler <sandro.spengler@web.de>
2022-03-01 19:27:49 +0100

    Production

--- scripts/runDockerCompose.sh
+++ scripts/runDockerCompose.sh
@@ -1,13 +1,11 @@
 docker-compose \
-    -f /home/sandro/Dropbox/0_Webdevelopment/Docker/Live/PersonalWebsite/docker-compose.yaml \
-    -f /home/sandro/Dropbox/0_Webdevelopment/Docker/Live/BackendServerMainDocker/docker-compose.yaml \
+    -f docker-compose.yaml \
+    -f docker-compose.prod.yaml \
     down --remove-orphans -v
 
 docker-compose \
     -f docker-compose.yaml \
-    -f docker-compose.dev.yaml \
-    -f /home/sandro/Dropbox/0_Webdevelopment/Docker/Live/BackendServerMainDocker/docker-compose.yaml \
-    -f /home/sandro/Dropbox/0_Webdevelopment/Docker/Live/BackendServerMainDocker/docker-compose.dev.yaml \
+    -f docker-compose.prod.yaml \
     up -d \
     --build \
 
